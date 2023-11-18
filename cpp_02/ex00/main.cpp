65d04c7e5333d4625b7d3c3b6c0e71316abb2701
Jens Kulka <kulkajens82@gmail.com>
2023-11-18 16:24:31 +0100

    Chore: removed binary file

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 12:32:43 by jkulka            #+#    #+#             */
-/*   Updated: 2023/11/18 13:56:33 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/11/18 16:23:53 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,4 @@ int main()
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
-    
 }
