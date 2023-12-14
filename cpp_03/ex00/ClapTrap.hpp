539b1a1453754bcd45708110bbe73b941dd7b3e5
Jens Kulka <kulkajens82@gmail.com>
2023-12-14 12:50:20 +0100

    Feature: Completed ex_00

--- cpp_03/ex00/ClapTrap.hpp
+++ cpp_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 09:46:07 by jkulka            #+#    #+#             */
-/*   Updated: 2023/12/14 09:48:01 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/12/14 12:32:33 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 #include <iostream>
 
-
+class ClapTrap
+{
+    private:
+        std::string _name;
+        int         _hp;
+        int         _ep;
+        int         _ap;
+    public:
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap &c_ClapTrap);
+        ClapTrap &operator=(const ClapTrap &c_ClapTrap);
+        ~ClapTrap();
+        void attack(const std::string &target);
+        void beRepaired(unsigned int amount);
+        void takeDamage(unsigned int amount);
+};
 
 #endif // CLAPTRAP_HPP
